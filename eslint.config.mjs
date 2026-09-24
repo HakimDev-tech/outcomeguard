2f231f0afce541665a45fb3f7f52c1c5792f8077
KOUMAD Abdelahkim <217803801+HakimDev-tech@users.noreply.github.com>
2026-09-24 13:24:00 +0200

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,15 @@
-export default [];
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts"
+  ])
+]);
+
+export default eslintConfig;
